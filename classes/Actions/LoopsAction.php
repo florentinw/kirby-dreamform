ee0c84ad7453acab3ed57c0e20e6841c741fe81f
Florentin Walter <acc@florentin.co>
2024-12-04 15:30:15 +0100

    Fixed LoopsAction: Subscribe Field no supports "Unset", "Yes" & "No"

--- classes/Actions/LoopsAction.php
+++ classes/Actions/LoopsAction.php
@@ -38,9 +38,14 @@ class LoopsAction extends Action
 						'subscribed'	=> [
 							'label' => t('dreamform.actions.loops.subscribed.label'),
 							'help' => t('dreamform.actions.loops.subscribed.help'),
-							'type' => 'toggle',
-							'default' => true,
+							'type' => 'toggles',
+							'default' => '',
 							'width' => '1/3',
+							'options' => [
+								'' => t('dreamform.actions.loops.subscribe.unset'),
+								'true' => t('dreamform.actions.loops.subscribe.yes'),
+								'false' => t('dreamform.actions.loops.subscribe.no'),
+							]
 						],
 						'fields' => [
 							'label' => t('dreamform.actions.loops.fields.label'),
@@ -94,7 +99,7 @@ class LoopsAction extends Action
 		// subscribe or update the user
 		$request = static::request('PUT', "/contacts/update", array_merge([
 			'email' => $email,
-			'subscribed' => $this->block()->subscribed()->toBool() ? true : null,
+			'subscribed' => $this->block()->subscribed()->toBool() ?? null,
 			'mailingLists' => $mailingLists
 		], $values));
 
